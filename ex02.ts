447160f25b062e94f95812de22d23de2e3fcd7fc
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-12 18:03:26 -0300

    Exercício 02 - B

--- ex02.ts
+++ ex02.ts
@@ -1,10 +1,10 @@
-let lista = new Array<Object> = [
+let lista: Object = [
     {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
     {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial"},
     {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
     {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}];
 
-    // a) Crie uma função que retorne a bio do id passado
+// a) Crie uma função que retorne a bio do id passado
 
 interface Pessoas {
     id: number;
@@ -26,4 +26,20 @@ function pegarBioPeloID(id: number): string | null {
     }
 }
 
-console.log(pegarBioPeloID(1))
+//console.log(pegarBioPeloID(1))
+
+//b) Crie uma função que retorne o name do id passado
+
+function pegarNomePeloID(id: number): string | null {
+    const pessoa = listaDePessoas.find((item) => item.id === id)
+
+    if (pessoa) {
+        return pessoa.name;
+    } else {
+        return null;
+    }
+}
+
+console.log(pegarNomePeloID(1))
+
+

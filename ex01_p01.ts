93ef54ecf74c457e4e9ae3fd1b1b37182acf7a3f
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-13 16:23:20 -0300

    Update ex01_p01.ts

--- ex01_p01.ts
+++ ex01_p01.ts
@@ -1,16 +1,16 @@
 function contarVogais(palavra: string) {
-    let letras: string = palavra
-    const palavraSeparada: string[] = letras.toLowerCase().split("")
-    let contador: number = 0
+    const letras: string = palavra;
+    const palavraSeparada: string[] = letras.toLowerCase().split("");
+    let contador: number = 0;
 
-    for(const a of palavraSeparada) {
+    for(let a of palavraSeparada) {
         if("aeiou".includes(a)) {
-            contador++
+            contador++;
         }
     }
 
-    return contador
+    return contador;
 }
 
-const resultado = contarVogais("Diego")
-console.log(`Numero de vogais é ${resultado}`)
+const resultado = contarVogais("Diego");
+console.log(`Numero de vogais é ${resultado}`);

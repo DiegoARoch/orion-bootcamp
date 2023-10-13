83d648721881d766cacbb44db4dde64c354a587b
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-13 16:24:30 -0300

    Exercício 01 - Update

--- ex01_p01.js
+++ ex01_p01.js
@@ -1,9 +1,9 @@
 "use strict";
 function contarVogais(palavra) {
-    let letras = palavra;
+    const letras = palavra;
     const palavraSeparada = letras.toLowerCase().split("");
     let contador = 0;
-    for (const a of palavraSeparada) {
+    for (let a of palavraSeparada) {
         if ("aeiou".includes(a)) {
             contador++;
         }

7e5fb55008fe896571ff58fa23f9185f3a2844a6
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-14 08:20:11 -0300

    Update

--- script.js
+++ script.js
@@ -17,26 +17,26 @@ function suporteParaOClique() {
         resultadoElemento.innerHTML = `Número de vogais é ${resultado}`;
     }
 }
-let lista = [
+//let lista = [];
+function criarTabela() {
+    const tabelaCorpo = document.getElementById('tabelaCorpo');
+    for (let i = 0; i < listaDePessoas.length; i++) {
+        const linha = tabelaCorpo.insertRow();
+        const cellId = linha.insertCell(0);
+        const cellNome = linha.insertCell(1);
+        const cellBio = linha.insertCell(2);
+        cellId.innerHTML = listaDePessoas[i]["id"].toString();
+        cellNome.innerHTML = listaDePessoas[i]["name"];
+        cellBio.innerHTML = listaDePessoas[i]["bio"];
+    }
+}
+window.onload = criarTabela;
+const listaDePessoas = [
     { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
     { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
     { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
     { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
-function criarTabela() {
-    const tabelaCorpo = document.getElementById('tabelaCorpo');
-    for (let i = 0; i < lista.length; i++) {
-        const linha = tabelaCorpo.insertRow();
-        const cellId = linha.insertCell(0);
-        const cellNome = linha.insertCell(1);
-        const cellBio = linha.insertCell(2);
-        cellId.innerHTML = lista[i]["id"].toString();
-        cellNome.innerHTML = lista[i]["name"];
-        cellBio.innerHTML = lista[i]["bio"];
-    }
-}
-window.onload = criarTabela;
-const listaDePessoas = [...lista];
 function pegarBioPeloID(id) {
     const pessoa = listaDePessoas.find((item) => item.id === id);
     if (pessoa) {
--- script.ts
+++ script.ts
@@ -22,26 +22,21 @@ function suporteParaOClique() {
 }
 
 
-let lista = [
-    { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
-    { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
-    { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
-    { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
-];
+//let lista = [];
 
 function criarTabela() {
     const tabelaCorpo= (<HTMLTableElement>document.getElementById('tabelaCorpo'));
 
 
-    for (let i = 0; i < lista.length; i++) {
+    for (let i = 0; i < listaDePessoas.length; i++) {
         const linha = tabelaCorpo.insertRow();
         const cellId = linha.insertCell(0);
         const cellNome = linha.insertCell(1);
         const cellBio = linha.insertCell(2);
 
-        cellId.innerHTML = lista[i]["id"].toString();
-        cellNome.innerHTML = lista[i]["name"];
-        cellBio.innerHTML = lista[i]["bio"];
+        cellId.innerHTML = listaDePessoas[i]["id"].toString();
+        cellNome.innerHTML = listaDePessoas[i]["name"];
+        cellBio.innerHTML = listaDePessoas[i]["bio"];
     }
 }
 
@@ -53,7 +48,12 @@ interface Pessoas {
     bio: string;
 }
 
-const listaDePessoas: Pessoas [] = [...lista]
+const listaDePessoas: Pessoas [] = [
+    { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
+    { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
+    { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
+    { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+]
 
 function pegarBioPeloID(id: number): string | null {
     const pessoa = listaDePessoas.find((item) => item.id === id);

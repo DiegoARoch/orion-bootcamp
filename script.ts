d1c88f4a52a84a1a3496ffeffe456a69b8667c67
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-14 03:10:40 -0300

    Exercício 3 - Update

---
+++ script.ts
@@ -0,0 +1,22 @@
+function contarVogais(palavra: string) {
+    const palavraSeparada: string[] = palavra.toLowerCase().split("");
+    let contador: number = 0;
+
+    for(let a of palavraSeparada) {
+        if("aeiou".includes(a)) {
+            contador++;
+        }
+    }
+
+    return contador;
+}
+
+function suporteParaOClique() {
+    const palavraInput = (document.getElementById('palavra') as HTMLInputElement).value;
+    const resultado: number = contarVogais(palavraInput);
+    const resultadoElemento = document.querySelector('div#resultado');
+
+    if (resultadoElemento) {
+        resultadoElemento.innerHTML = `Número de vogais é ${resultado}`;
+    }
+}

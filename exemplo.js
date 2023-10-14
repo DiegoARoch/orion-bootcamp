d700697111c1b265d2ac264e5550e5e8862ccb35
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-14 03:52:56 -0300

    Update

---
+++ exemplo.js
@@ -0,0 +1,125 @@
+"use strict";
+let lista = [
+    { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
+    { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
+    { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
+    { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+];
+const listaDePessoas = [...lista];
+//console.log(listaDePessoas)
+function pegarBioPeloID(id) {
+    const pessoa = listaDePessoas.find((item) => item.id === id); //funcional
+    if (pessoa) { // Imperativo
+        return pessoa.bio;
+    }
+    else {
+        return null;
+    }
+}
+//console.log(pegarBioPeloID(1))
+//b) Crie uma função que retorne o name do id passado
+function pegarNomePeloID(id) {
+    const pessoa = listaDePessoas.find((item) => item.id === id); //funcional
+    if (pessoa) { // Imperativo
+        return pessoa.name;
+    }
+    else {
+        return null;
+    }
+}
+//console.log(pegarNomePeloID(1))
+//c) Crie uma função que apague um item da lista a partir de um id passado
+function apagarItemPeloID(id) {
+    for (let i = 0; i < listaDePessoas.length; i++) { //imperativo
+        if (listaDePessoas[i].id === id) {
+            listaDePessoas.splice(i, 1); //splice funcional
+            break;
+        }
+    }
+}
+//apagarItemPeloID(2)
+//console.log(listaDePessoas)
+// d) Crie uma função que altere a bio ou o name a partir de um id passado
+function alterarItemPeloID(id, novoNome, novaBio) {
+    const pessoa = listaDePessoas.find((item) => item.id === id); //funcional
+    if (pessoa) { //imperativo
+        if (novoNome !== undefined) {
+            pessoa.name = novoNome;
+        }
+        if (novaBio !== undefined) {
+            pessoa.bio = novaBio;
+        }
+    }
+}
+//const bioClapton: string = "Eric Clapton é um renomado músico britânico nascido em 30 de março de 1945 em Ripley, Surrey, Inglaterra. Ele é amplamente considerado um dos maiores guitarristas de todos os tempos e é conhecido por sua carreira solo de sucesso, bem como por seu trabalho com várias bandas e músicos notáveis."
+//alterarItemPeloID(2, undefined, bioClapton)
+//console.log(listaDePessoas)
+//alterarItemPeloID(2, "Eric Clapton", bioClapton)
+//console.log(listaDePessoas)
+// e) Demonstre todas as funções com o paradigma funcional e com o imperativo
+//QUESTÂO A - FUNCIONAL
+function pegarBioPeloIDFuncional(id) {
+    const pessoa = listaDePessoas.find((item) => item.id === id);
+    return pessoa ? pessoa.bio : null;
+}
+//QUESTÃO A - IMPERATIVO
+function pegarBioPeloIDImperativo(id) {
+    for (let i = 0; i < listaDePessoas.length; i++) {
+        if (listaDePessoas[i].id === id) {
+            return listaDePessoas[i].bio;
+        }
+    }
+    return null;
+}
+//QUESTÃO B - FUNCIONAL
+function pegarNomePeloIDFuncional(id) {
+    const pessoa = listaDePessoas.find((item) => item.id === id);
+    return pessoa ? pessoa.name : null;
+}
+//QUESTÃO B - IMPERATIVO
+function pegarNomePeloIDImperativo(id) {
+    for (let i = 0; i < listaDePessoas.length; i++) {
+        if (listaDePessoas[i].id === id) {
+            return listaDePessoas[i].name;
+        }
+    }
+    return null;
+}
+//QUESTÃO C - FUNCIONAL
+function apagarItemPeloIDFuncional(id) {
+    const listaRemover = listaDePessoas.findIndex((pessoa) => pessoa.id === id);
+    listaRemover !== -1 && listaDePessoas.splice(listaRemover, 1);
+}
+//QUESTÃO C - IMPERATIVO
+function apagarItemPeloIDImperativo(id) {
+    for (let i = 0; i < listaDePessoas.length; i++) {
+        if (listaDePessoas[i].id === id) {
+            for (let j = i; j < listaDePessoas.length - 1; j++) {
+                listaDePessoas[j] = listaDePessoas[j + 1];
+            }
+            listaDePessoas.length--;
+            break;
+        }
+    }
+}
+//QUESTÃO D - FUNCIONAL
+function alterarItemPeloIDFuncional(id, novoNome, novaBio) {
+    const pessoaIndex = listaDePessoas.findIndex((item) => item.id === id);
+    if (pessoaIndex !== -1) {
+        listaDePessoas[pessoaIndex] = Object.assign(Object.assign({}, listaDePessoas[pessoaIndex]), { name: novoNome !== null && novoNome !== void 0 ? novoNome : listaDePessoas[pessoaIndex].name, bio: novaBio !== null && novaBio !== void 0 ? novaBio : listaDePessoas[pessoaIndex].bio });
+    }
+}
+//QUESTÃO D - IMPERATIVO
+function alterarItemPeloIDImperativo(id, novoNome, novaBio) {
+    for (let i = 0; i < listaDePessoas.length; i++) {
+        if (listaDePessoas[i].id === id) {
+            if (novoNome !== undefined) {
+                listaDePessoas[i].name = novoNome;
+            }
+            if (novaBio !== undefined) {
+                listaDePessoas[i].bio = novaBio;
+            }
+            break;
+        }
+    }
+}
--- script.ts
+++ script.ts
@@ -20,3 +20,81 @@ function suporteParaOClique() {
         resultadoElemento.innerHTML = `Número de vogais é ${resultado}`;
     }
 }
+
+let lista: Object = [
+    { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
+    { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
+    { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
+    { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+];
+
+function criarTabela() {
+    const tabelaCorpo = document.getElementById('tabelaCorpo');
+
+    for (let i = 0; i < lista.length; i++) {
+        const linha = tabelaCorpo.insertRow();
+        const cellId = linha.insertCell(0);
+        const cellNome = linha.insertCell(1);
+        const cellBio = linha.insertCell(2);
+
+        cellId.innerHTML = lista[i]["id"].toString();
+        cellNome.innerHTML = lista[i]["name"];
+        cellBio.innerHTML = lista[i]["bio"];
+    }
+}
+
+window.onload = criarTabela;
+
+interface Pessoas {
+    id: number;
+    name: string;
+    bio: string;
+}
+
+const listaDePessoas : Pessoas[] = [...lista]
+
+function pegarBioPeloID(id: number): string | null {
+    const pessoa = listaDePessoas.find((item) => item.id === id);
+
+    if (pessoa) {
+        return pessoa.bio;
+    } else {
+        return null;
+    }
+}
+
+function consultarBiografia() {
+    const consultaId = parseInt((document.getElementById('consultaId') as HTMLInputElement).value);
+    const consultaResultado = document.getElementById('consultaResultado');
+
+    const biografia = pegarBioPeloID(consultaId);
+
+    if (biografia) {
+        consultaResultado.innerHTML = `Biografia: ${biografia}`;
+    } else {
+        consultaResultado.innerHTML = "Pessoa não encontrada.";
+    }
+}
+
+function pegarNomePeloID(id: number): string | null {
+    const pessoa = listaDePessoas.find((item) => item.id === id);
+
+    if (pessoa) {
+        return pessoa.name;
+    } else {
+        return null;
+    }
+}
+
+function consultarNomePorID() {
+    const consultaNomeId = parseInt((document.getElementById('consultaNomeId') as HTMLInputElement).value);
+    const consultaNomeResultado = document.getElementById('consultaNomeResultado');
+
+    const nome = pegarNomePeloID(consultaNomeId);
+
+    if (nome) {
+        consultaNomeResultado.innerHTML = `Nome: ${nome}`;
+    } else {
+        consultaNomeResultado.innerHTML = "Pessoa não encontrada.";
+    }
+}

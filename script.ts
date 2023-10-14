fd4d920df3d27980b6a201cde4ef6af61bfd044a
DiegoARoch <118547895+DiegoARoch@users.noreply.github.com>
2023-10-14 07:46:05 -0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -21,8 +21,8 @@ function suporteParaOClique() {
     }
 }
 
-// PROBLEMA COM ESSA TIPAGEM AQUI!!!
-let lista: Array = [
+
+let lista: Pessoas[] = [
     { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
     { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
     { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
@@ -53,7 +53,7 @@ interface Pessoas {
     bio: string;
 }
 
-const listaDePessoas : Pessoas[] = [...lista]
+const listaDePessoas = [...lista]
 
 function pegarBioPeloID(id: number): string | null {
     const pessoa = listaDePessoas.find((item) => item.id === id);
